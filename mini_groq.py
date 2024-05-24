4e7105d89bc9f2fe6e49b082b436620d4055e639
joker92000 <hosny.kasmi@gmail.com>
2024-05-24 15:47:40 +0200

    modif mini_groq1

--- mini_groq.py
+++ mini_groq.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_URL = "https://api.groq.com/v1/endpoint"  # Remplacez par l'URL correcte de l'API Groq
-GROQ_API_TOKEN = os.getenv("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")
+GROQ_API_TOKEN = os.getenv("GROQ_API_KEY")
 
 async def call_groq_api(prompt: str):
     headers = {
